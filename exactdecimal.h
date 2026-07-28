ba54257b4670a9cd7e490845260149bf7d2be5aa
LuisKiw <LuisinElPillin4@outlook.es>
2026-07-28 20:28:31 +0200

    exactdecimal.h Comment Commit

--- exactdecimal.h
+++ exactdecimal.h
@@ -3,12 +3,15 @@
 #include<string>
 
 void CalculateFloat(bool ready, float a, float b) {
-    //security check
+    //bool check
     if (ready == true) 
     {
+        //converting the floats into integers
         int IntA = a * 100;
         int IntB = b * 100;
+        //adding the integers
         int ResultInt = IntA + IntB;
+        //printing the result
         std::cout << "0." << ResultInt << std::string(20, '0') << "\n";
     }
     else
