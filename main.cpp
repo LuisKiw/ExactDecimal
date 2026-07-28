e03ac75d47a5ca89eac99617f710ba87847346f3
LuisKiw <LuisinElPillin4@outlook.es>
2026-07-28 20:24:06 +0200

    main.cpp Comments Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+//main.cpp
+//not really important but necessary
 #include<iostream>
 #include "exactdecimal.h"
 
 int main(){
+    //creating the floats
     float a = 0.1;
     float b = 0.2;
+    //useless bool, I might delete it
     bool ready = true;
     CalculateFloat(ready, a, b);
     return 0;
